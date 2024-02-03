89b35acbeab0a49af229da2c7759fe43e7ce8a04
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-03 12:19:48 -0800

    first test successful fail

--- src/__tests__/reducers/item-list-reducer.test.js
+++ src/__tests__/reducers/item-list-reducer.test.js
@@ -1,7 +1,39 @@
 import itemListReducer from '../../reducers/item-list-reducer';
 
 describe('itemListReducer', () => {
+
+  let action;
+  const itemData = {
+    id: 1,
+    flavor: 'Chocolate',
+    price: 0.75, 
+    inStock: 0, 
+    popularity: "High"
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(itemListReducer({}, { type: null })).toEqual({});
+    expect(itemListReducer([], { type: null })).toEqual([]);
   });
+
+  test('Should successfully add new item data to itemToAddStock array', () => {
+    const { id, flavor, price, inStock, popularity } = itemData;
+    action = { 
+      type: 'ADD_ITEM',
+      id: id,
+      flavor: flavor,
+      price: price, 
+      inStock: inStock, 
+      popularity: popularity
+    };
+
+    expect(itemListReducer([], action)).toEqual([
+      {
+        id: id,
+        flavor: flavor,
+        price: price, 
+        inStock: inStock, 
+        popularity: popularity
+      }
+    ])
+  })
 });
