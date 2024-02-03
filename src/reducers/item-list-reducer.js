3f24c4890a23682556c209e91bc4c378c63b558d
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-03 12:23:51 -0800

    first test passed

--- src/reducers/item-list-reducer.js
+++ src/reducers/item-list-reducer.js
@@ -1,5 +1,20 @@
-const reducer = (state = {}, action) => {
-
+const reducer = (state = [], action) => {
+  const { id, flavor, price, inStock, popularity } = action;
+  switch (action.type) {
+    case 'ADD_ITEM': 
+      return [
+        ...state,
+        {
+          id: id,
+          flavor: flavor,
+          price: price,
+          inStock: inStock,
+          popularity: popularity
+        }
+      ];
+    default: 
+      return state;
+  }
 };
 
 export default reducer;
